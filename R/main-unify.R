65ac142a31eaafbf55d3d82ba128eead4fef4b87
Addison Torres Grant <addisong@sccwrp.org>
2025-07-07 13:51:53 -0700

    Reorganize R scripts into own directory

--- R/main-unify.R
+++ R/main-unify.R
@@ -4,7 +4,7 @@ years = c("1994", "1998", "2003", "2008", "2013", "2018", "2023")
 
 for (y in years) {
   print(paste("Processing year", y))
-  callr::rscript(paste0("R/unify-summary-", y, ".R"))
+  callr::rscript(paste0("R/unify/unify-summary-", y, ".R"))
 }
 
-callr::rscript("R/unify-summaries.R")
+callr::rscript("R/unify/unify-summaries.R")
