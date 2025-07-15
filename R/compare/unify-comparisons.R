7918bbaea3ffe68708446ad989e973c539079910
Addison Torres Grant <addisong@sccwrp.org>
2025-07-15 11:45:44 -0700

    Commit no-op scripts for '94 and '98
    Probably don't have enough published summary columns for proper comparison.

--- R/compare/unify-comparisons.R
+++ R/compare/unify-comparisons.R
@@ -1,6 +1,6 @@
 make_path = function(year) { paste0("data/compare-", year, ".rds") }
 
-years = c("2008", "2013", "2018", "2023")
+years = c("2003", "2008", "2013", "2018", "2023")
 
 datasets = list()
 for (y in years) {
--- R/main-compare.R
+++ R/main-compare.R
@@ -2,7 +2,7 @@
 output_columns = names(readr::read_csv("data-raw/from-bight2023-db/tbl_toxsumaryresultspublish_unifiedread.csv"))
 readr::write_rds(output_columns, "data/output_columns.rds")
 
-years = c("2003", "2008", "2013", "2018", "2023")
+years = c("1994", "1998", "2003", "2008", "2013", "2018", "2023")
 
 for (y in years) {
   print(paste("Processing year", y))
